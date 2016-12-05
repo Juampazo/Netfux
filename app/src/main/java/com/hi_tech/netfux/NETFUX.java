a0b7557d13f0fd7d1fb5de666cfbc932569383ab
Juan Pablo Arias Reyes <juparias993@hotmail.com>
2016-12-04 23:58:34 -0400

    Iniciando la app

---
+++ app/src/main/java/com/hi_tech/netfux/NETFUX.java
@@ -0,0 +1,13 @@
+package com.hi_tech.netfux;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NETFUX extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_netfux);
+    }
+}
