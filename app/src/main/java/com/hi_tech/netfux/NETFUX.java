2a2685062ff345ca340066b57f7e1e8fcb954504
Juan Pablo Arias Reyes <juparias993@hotmail.com>
2016-12-06 00:32:54 -0400

    Estilo cambiado / slide añadido por falta de control sobre el horizontal scroll view / Color de la toolbar cambiado por falta de control sobre el color del texto de la toolbar

--- app/src/main/java/com/hi_tech/netfux/NETFUX.java
+++ app/src/main/java/com/hi_tech/netfux/NETFUX.java
@@ -1,7 +1,10 @@
 package com.hi_tech.netfux;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
 
 public class NETFUX extends AppCompatActivity {
 
